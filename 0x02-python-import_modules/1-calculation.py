484621c304ddb3e6c612475a45bc2f0916d287d1
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-10-05 10:08:52 -0700

    new line

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
+if __name__ == "__main__":
     """Prints some mathmatical opperations"""
-    from caclulator_1 import add,sub,mul,div
+    from calculator_1 import add,sub,mul,div
     a = 10
     b = 5
     print("{} + {} = {}" .format(a, b, add(a,b)))
-    print("{} - {} = {}" .fomrat(a, b, sub(a,b)))
-    print("{} * {} = {}" .fomrat(a, b, mul(a,b)))
-    print("{} / {} = {}" .fomrat(a, b, div(a,b)))
+    print("{} - {} = {}" .format(a, b, sub(a,b)))
+    print("{} * {} = {}" .format(a, b, mul(a,b)))
+    print("{} / {} = {}" .format(a, b, div(a,b)))
