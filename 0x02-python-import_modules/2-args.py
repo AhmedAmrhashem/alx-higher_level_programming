402033a54de1b0f1837f203f4827ab11698c1a74
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-10-05 10:44:56 -0700

    task3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    counter = len(sys.argv)
+    if counter == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:" .format(counter))
+        for i in range(1, counter):
+            print("{} : {}" .format(i, sys.argv[i]))
